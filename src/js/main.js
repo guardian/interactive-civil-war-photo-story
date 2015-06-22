2762937ee9f4879d440b3b8c6c696d3b7449aa71
daanlouter <daan.louter@gmail.com>
2015-06-22 16:26:56 +0100

    changes before launch

--- src/js/main.js
+++ src/js/main.js
@@ -78,7 +78,7 @@ define([
 
     function loadData(params){
         var key = "1r7dnLeNi9RhDQeB5VWqvskSw4GK7FkO1nUV1rX8XWjM";
-        liveLoad = true;
+        liveLoad = false;
         if(!liveLoad){
             get('http://interactive.guim.co.uk/spreadsheetdata/'+key+'.json')
                 .then(JSON.parse)
